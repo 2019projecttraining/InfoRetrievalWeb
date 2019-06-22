713e7a13a42648cd9fbfba292207509d63f90c2d
余定邦 <876633022@qq.com>
2019-06-22 10:37:19 +0800

    添加lucene部分

---
+++ IR_WEB/src/main/java/ir/luceneIndex/LuceneSearcher.java
@@ -0,0 +1,39 @@
+package ir.luceneIndex;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LuceneSearcher extends IndexSearcher{
+	
+	public final static String LUCENE_INDEX_FILE_PATH="";
+	
+	static {
+		//指定索引库存放路径
+		//E:\Lucene_index
+		Directory directory;
+		try {
+			directory = FSDirectory.open(Paths.get(new File(LUCENE_INDEX_FILE_PATH).getPath()));
+			//创建indexReader对象
+			indexReader = DirectoryReader.open(directory);
+			//创建indexSearcher对象
+		} catch (IOException e) {
+			throw new RuntimeException("Lucene 索引读取失败");
+		}
+	}
+	
+	private final static IndexReader indexReader;
+
+	public LuceneSearcher() throws IOException{
+		super(indexReader);
+	}
+	
+}
--- IR_WEB/src/main/java/ir/services/impl/SearchServiceImpl.java
+++ IR_WEB/src/main/java/ir/services/impl/SearchServiceImpl.java
@@ -1,10 +1,16 @@
 package ir.services.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import ir.luceneIndex.LuceneSearcher;
 import ir.models.PatentsForView;
 import ir.services.SearchService;
 
 public class SearchServiceImpl implements SearchService{
 
+	@Autowired
+	private LuceneSearcher luceneSearcher;
+	
 	@Override
 	public PatentsForView search(String keyWords) {
 		// TODO Auto-generated method stub
