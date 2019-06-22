1bc4375baec3f5093bc42a7c060b4e22cce773e7
18355010305 <37801179+18355010305@users.noreply.github.com>
2019-06-22 16:08:01 +0800

    专利实体类修改、注释及专利前端传值类编写

--- IR_WEB/src/main/java/ir/models/Patent.java
+++ IR_WEB/src/main/java/ir/models/Patent.java
@@ -16,51 +16,88 @@ import lombok.Data;
 @Table(name="t_patents",uniqueConstraints= {@UniqueConstraint(columnNames= {"id"})})
 public class Patent {
 
+	/**
+	 * 专利条目编号
+	 */
 	@Id
 	@GenericGenerator(name="generator",strategy="assigned")
 	@GeneratedValue(generator="generator")
 	@Column(name="id")
 	private String id;
 	
+	/**
+	 * 专利摘要
+	 */
 	@Column(name="abstract")
 	private String patent_Abstract;
 	
+	/**
+	 * 专利地址
+	 */
 	@Column(name="addreass")
 	private String address;
 	
+	/**
+	 * 专利申请单位、个人
+	 */
 	@Column(name="applicant")
 	private String applicant;
 	
+	
+	/**
+	 * 公开日期
+	 */
 	@Column(name="application_date")
 	private String application_date;
 	
+	/**
+	 * 申请号码
+	 */
 	@Column(name="application_number")
 	private String application_number;
 	
+	/**
+	 * 公开号码
+	 */
 	@Column(name="application_publish_number")
 	private String application_publish_number;
 	
+	/**
+	 * 分类号
+	 */
 	@Column(name="classification_number")
 	private String classification_number;
 	
+	/**
+	 * 申请日期
+	 */
 	@Column(name="filling_date")
 	private String filling_date;
 	
+	/**
+	 * 授权状态
+	 */
 	@Column(name="grant_status")
 	private int grant_status;
 	
+	/**
+	 * 发明者
+	 */
 	@Column(name="inventor")
 	private String inventor;
 
-	
+	/**
+	 * 标题
+	 */
 	@Column(name="title")
 	private String title;
 	
+	/**
+	 * 年份
+	 */
 	@Column(name="year")
 	private int year;
 	
-	@Column(name="grant_odd")
-	private double grant_odd;
 
 	@Override
 	public String toString() {
@@ -68,7 +105,7 @@ public class Patent {
 				+ applicant + ", application_date=" + application_date + ", application_number=" + application_number
 				+ ", application_publish_number=" + application_publish_number + ", classification_number="
 				+ classification_number + ", filling_date=" + filling_date + ", grant_status=" + grant_status
-				+ ", inventor=" + inventor + ", title=" + title + ", year=" + year + ", grant_odd=" + grant_odd + "]";
+				+ ", inventor=" + inventor + ", title=" + title + ", year=" + year +"]";
 	}
 
 	
--- IR_WEB/src/main/java/ir/models/PatentsForView.java
+++ IR_WEB/src/main/java/ir/models/PatentsForView.java
@@ -1,7 +1,18 @@
 package ir.models;
 
+import java.util.List;
+
 public class PatentsForView {
 
 	//传递前端页面需要的值
-	
+	private List<Patent> patents;
+
+	public List<Patent> getPatents() {
+		return patents;
+	}
+
+	public void setPatents(List<Patent> patents) {
+		this.patents = patents;
+	}
+
 }
