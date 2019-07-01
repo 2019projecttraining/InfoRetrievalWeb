3c7d20dc8b5a3a9a66a7fc8e5724538e8501b6b8
tuibukaio <45137690+tuibukaio@users.noreply.github.com>
2019-07-01 16:34:14 +0800

    修改了切分方法
    可以根据输入修改建立索引方法
    修改了索引存放路径
    Lucene_index中分出四个子目录
    fuzzy_index模糊查询细力度分词目录
    accurate_index较精确查询粗粒度分词
    single_index单字符分词
    double_index双字符分词

--- src/main/java/ir/luceneIndex/Indexer.java
+++ src/main/java/ir/luceneIndex/Indexer.java
@@ -1,22 +1,19 @@
 package ir.luceneIndex;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 
-import javax.management.Query;
-
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.csv.*;
-import org.apache.commons.csv.CSVFormat.Predefined;
-import org.apache.lucene.analysis.cn.smart.SmartChineseAnalyzer;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.cjk.CJKAnalyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.FieldType;
-import org.apache.lucene.document.IntPoint;
-import org.apache.lucene.document.StringField;
-import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexOptions;
 import org.apache.lucene.index.IndexReader;
@@ -25,7 +22,6 @@ import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.search.BooleanClause.Occur;
 import org.apache.lucene.search.BooleanQuery;
-import org.apache.lucene.search.FuzzyQuery;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TermQuery;
@@ -35,148 +31,64 @@ import org.apache.lucene.store.FSDirectory;
 
 import com.huaban.analysis.jieba.JiebaSegmenter;
 
+import ir.config.Configuration;
+import ir.enumDefine.SearchAccuracy;
 import ir.util.seg.jieba.JiebaAnalyzer;
 import net.sourceforge.pinyin4j.PinyinHelper;
 
 public class Indexer {
 	/**
-	 * 写入索引文件 jieba分词的index模式
+	 * 按照不同的索引文件写入对应索引目录
 	 * 
 	 * @author WC
 	 */
-	public static void luceneCreateIndex() throws Exception {
-		// 指定索引存放的位置
-		Directory directory = FSDirectory.open(Paths.get(new File("D:\\Lucene_Path\\Lucene_index").getPath()));
-		System.out.println("pathname" + Paths.get(new File("D:\\Lucene_Path\\Lucene_index").getPath()));
-		// 创建一个分词器
-		// StandardAnalyzer analyzer = new StandardAnalyzer();
-		// CJKAnalyzer cjkAnalyzer = new CJKAnalyzer();
-		// 智能中文分词器
-		// SmartChineseAnalyzer smartChineseAnalyzer = new SmartChineseAnalyzer();
-		JiebaAnalyzer jiebaAnalyzer = new JiebaAnalyzer(JiebaSegmenter.SegMode.INDEX);
-		// 创建indexwriterConfig(参数分词器)
-		IndexWriterConfig indexWriterConfig = new IndexWriterConfig(jiebaAnalyzer);
-		// 创建indexwriter对象(文件对象,索引配置对象)
-		IndexWriter indexWriter = new IndexWriter(directory, indexWriterConfig);
-		// 读csv
-		String[] headers = new String[] { "id", "abstract", "address", "applicant", "application_date",
-				"application_number", "application_publish_number", "classification_number", "filing_date",
-				"grant_status", "inventor", "title", "year" };
-		String csvPath = "D:\\Lucene_Path\\newpatent.csv";
+	public static void luceneCreateIndex(SearchAccuracy search) throws Exception {
+		// 默认指定索引存放的位置
+		String path = Configuration.getConfig("fuzzy-lucene-index-file-path");
 
-		FileReader reader = new FileReader(csvPath);
-		CSVParser parser = CSVFormat.DEFAULT.withHeader(headers).parse(reader);
+		// System.out.println("pathname" + Paths.get(new
+		// File(Configuration.getConfig("fuzzy-lucene-index-file-path")).getPath()));
+		// jieba切分 注意index模式(分词粗)和search模式(相对细)
+		// JiebaAnalyzer jiebaAnalyzer = new
+		// JiebaAnalyzer(JiebaSegmenter.SegMode.INDEX);
+		Analyzer analyzer = new JiebaAnalyzer(JiebaSegmenter.SegMode.INDEX);
 
-		for (CSVRecord record : parser) {
-
-			System.out.println(record.get("title"));
-			// preserved_type保护域不分词
-			// default_type默认分词
-			// 分词模式DOCS
-			FieldType preserved_type = new FieldType();
-			preserved_type.setTokenized(false);
-			preserved_type.setStored(true);
-			preserved_type.setIndexOptions(IndexOptions.DOCS);
-			FieldType default_type = new FieldType();
-			default_type.setTokenized(true);
-			default_type.setStored(true);
-			default_type.setIndexOptions(IndexOptions.DOCS);
-
-			// 创建文件域名
-			// Field(域的名称,域的内容,fieldtype)
-			Field id_field = new Field("id", record.get("id"), preserved_type);
-			Field abstract_field = new Field("abstract", record.get("abstract"), default_type);
-			Field address_field = new Field("address", record.get("address"), default_type);
-			Field applicant_field = new Field("applicant", record.get("applicant"), preserved_type);
-
-			Field application_date_field = new Field("application_date", record.get("application_date"),
-					preserved_type);
-			Field application_number_field = new Field("application_number", record.get("application_number"),
-					preserved_type);
-			Field application_publish_number_field = new Field("application_publish_number",
-					record.get("application_publish_number"), preserved_type);
-
-			Field classification_number_field = new Field("classification_number", record.get("classification_number"),
-					preserved_type);
-			Field filing_date_field = new Field("filing_date", record.get("filing_date"), preserved_type);
-			Field grant_status_field = new Field("grant_status", record.get("grant_status"), preserved_type);
-
-			// Field inventor_field = new Field("inventor", record.get("inventor"),
-			// preserved_type);
-			Field title_field = new Field("title", record.get("title"), default_type);
-			Field year_field = new Field("year", record.get("year"), preserved_type);
-
-			Document indexableFields = new Document();
-			indexableFields.add(id_field);
-			indexableFields.add(abstract_field);
-			indexableFields.add(address_field);
-			indexableFields.add(applicant_field);
-
-			indexableFields.add(application_date_field);
-			indexableFields.add(application_number_field);
-			indexableFields.add(application_publish_number_field);
-
-			indexableFields.add(classification_number_field);
-			indexableFields.add(filing_date_field);
-			indexableFields.add(grant_status_field);
-
-			// indexableFields.add(inventor_field);
-			indexableFields.add(title_field);
-			indexableFields.add(year_field);
-
-			// 添加姓名域以及首字母域
-			// 姓名域改为多值域
-			String inventorS = record.get("inventor");
-			String inventor[] = inventorS.split(";");
-			for (String out : inventor) {
-				String firstW = "";
-				System.out.println(out);
-				if (out.charAt(0) >= 'A' && out.charAt(0) <= 'Z' || out.charAt(0) >= 'a' && out.charAt(0) <= 'z') {
-					firstW = String.valueOf(out.charAt(0));
-					// System.out.println(firstW);
-				} else {
-					String temp[] = PinyinHelper.toHanyuPinyinStringArray(out.charAt(0));
-					if (temp != null)
-						firstW = String.valueOf(temp[0].charAt(0));
-					System.out.println(firstW);
-				}
-				firstW = firstW.toUpperCase();
-				// 姓名域
-				Field inventor_field = new Field("inventor", out, preserved_type);
-				// 首字母域FirstWorld
-				Field inventor_firstW_field = new Field("inventor_firstW", firstW, preserved_type);
-
-				indexableFields.add(inventor_field);
-				indexableFields.add(inventor_firstW_field);
-			}
-
-			// indexableFields.add(fileSizeField);
-			// 创建索引 并写入索引库
-			indexWriter.addDocument(indexableFields);
-
+		// 判断分词器选择
+		switch (search) {
+		case FUZZY:
+			analyzer = new JiebaAnalyzer(JiebaSegmenter.SegMode.INDEX);
+			path = Configuration.getConfig("fuzzy-lucene-index-file-path");
+			break;
+		case ACCURATE:
+			analyzer = new JiebaAnalyzer(JiebaSegmenter.SegMode.SEARCH);
+			path = Configuration.getConfig("accurate-lucene-index-file-path");
+			break;
+		case SINGLE_WORD:
+			// 单字符切分
+			analyzer = new StandardAnalyzer();
+			path = Configuration.getConfig("single-word-lucene-index-file-path");
+			break;
+		case DOUBLE_WORD:
+			// 双字符切分
+			analyzer = new CJKAnalyzer();
+			path = Configuration.getConfig("double-word-lucene-index-file-path");
+			break;
+		default:
+			break;
 		}
-		reader.close();
-		// 关闭indexWriter
-		indexWriter.close();
-	}
 
-	/**
-	 * 建立新索引文件 jieba分词的search模式
-	 * 
-	 * @author WC
-	 */
-	public static void luceneCreateIndex_jiebasearchmode() throws Exception {
-		// 指定新索引存放的位置
-		Directory directory = FSDirectory.open(Paths.get(new File("D:\\Lucene_Path\\Lucene_jiebasearch").getPath()));
-		System.out.println("pathname" + Paths.get(new File("D:\\Lucene_Path\\Lucene_jiebasearch").getPath()));
-		// 创建一个分词器
+		// 默认fuzzy查询 jieba index模式
+		Directory directory = FSDirectory.open(Paths.get(new File(path).getPath()));
+
+		// 从下列分词器创建一个分词器
+		// 单字符切分
 		// StandardAnalyzer analyzer = new StandardAnalyzer();
+		// 双字符切分
 		// CJKAnalyzer cjkAnalyzer = new CJKAnalyzer();
 		// 智能中文分词器
 		// SmartChineseAnalyzer smartChineseAnalyzer = new SmartChineseAnalyzer();
-		JiebaAnalyzer jiebaAnalyzer = new JiebaAnalyzer(JiebaSegmenter.SegMode.SEARCH);
 		// 创建indexwriterConfig(参数分词器)
-		IndexWriterConfig indexWriterConfig = new IndexWriterConfig(jiebaAnalyzer);
+		IndexWriterConfig indexWriterConfig = new IndexWriterConfig(analyzer);
 		// 创建indexwriter对象(文件对象,索引配置对象)
 		IndexWriter indexWriter = new IndexWriter(directory, indexWriterConfig);
 		// 读csv
@@ -342,13 +254,34 @@ public class Indexer {
 	}
 
 	// 单域多条件查询
-	public static void multi_search(String field, String[] content, int dir) throws IOException {
+	public static void multi_search(String field, String[] content, SearchAccuracy search) throws IOException {
 		// 指定索引库存放路径
-		// D:\Lucene_Path\Lucene_index
-		Directory directory = FSDirectory.open(Paths.get(new File("D:\\Lucene_Path\\Lucene_index\\fuzzy_index").getPath()));
-		if (dir == 1) {
-			directory = FSDirectory.open(Paths.get(new File("D:\\Lucene_Path\\Lucene_index\\accurate_index").getPath()));
+		// 默认指定索引存放的位置
+		String path = Configuration.getConfig("fuzzy-lucene-index-file-path");
+
+		// 判断分词器路径选择
+		switch (search) {
+		case FUZZY:
+			path = Configuration.getConfig("fuzzy-lucene-index-file-path");
+			break;
+		case ACCURATE:
+			path = Configuration.getConfig("accurate-lucene-index-file-path");
+			break;
+		case SINGLE_WORD:
+			// 单字符切分
+			path = Configuration.getConfig("single-word-lucene-index-file-path");
+			break;
+		case DOUBLE_WORD:
+			// 双字符切分
+			path = Configuration.getConfig("double-word-lucene-index-file-path");
+			break;
+		default:
+			break;
 		}
+
+		// 默认fuzzy查询 jieba index模式
+		Directory directory = FSDirectory.open(Paths.get(new File(path).getPath()));
+
 		// 创建indexReader对象
 		IndexReader indexReader = DirectoryReader.open(directory);
 		// 创建indexSearcher对象
@@ -418,8 +351,11 @@ public class Indexer {
 
 	// 5bd935865e4c120b8c9787e3
 	public static void main(String[] args) throws Exception {
-		// luceneCreateIndex_jiebasearchmode();
-		String[] content = { "无线" };
-		multi_search("abstract", content, 1);
+		luceneCreateIndex(SearchAccuracy.FUZZY);
+		luceneCreateIndex(SearchAccuracy.ACCURATE);
+		luceneCreateIndex(SearchAccuracy.SINGLE_WORD);
+		luceneCreateIndex(SearchAccuracy.DOUBLE_WORD);
+		// String[] content = { "无","线","电","脑","鼠","标" };
+		// multi_search("title", content, 2);
 	}
 }
